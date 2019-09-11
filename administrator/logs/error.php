54a89bc71d048ad2354f98368bcfd495cfa71a67
Your Name <you@example.com>
2019-09-11 09:55:17 +0800

    e-commerce

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-09-10 02:04:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-09-10T02:04:37+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed.
---
+++ administrator/logs/upload.error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-09-08 09:46:31 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-09-08T09:46:31+00:00	INFO 127.0.0.1	upload	
+2019-09-08T09:46:43+00:00	INFO 127.0.0.1	upload	
+2019-09-08T09:47:01+00:00	INFO 127.0.0.1	upload	File exists: C:\Program Files (x86)\Zend\Apache24\htdocs\images\7822138276_791589993.jpg by user_id 536
+2019-09-08T09:47:12+00:00	INFO 127.0.0.1	upload	File exists: C:\Program Files (x86)\Zend\Apache24\htdocs\images\7833110598_791589993.jpg by user_id 536
+2019-09-08T09:47:20+00:00	INFO 127.0.0.1	upload	
